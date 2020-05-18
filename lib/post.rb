304651a0d8c17fe578ff5859860413ddb54e9926
kiku ishimoto <kikuish@gmail.com>
2020-05-18 22:38:21 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+  attr_accessor :name, :author
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    author.name if author
+
+  end
+
+
+end
